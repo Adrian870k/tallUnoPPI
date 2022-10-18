b6b2aa1113e253ccc3acb1699f59dc5532293f22
yor <montoyaquinjordan@gmail.com>
2022-10-18 00:00:39 -0500

    cambios

--- src/main/java/com/yod/taller/controller/UsuarioController.java
+++ src/main/java/com/yod/taller/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.yod.taller.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.yod.taller.services.dto.UsuarioDTO;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UsuarioController {
